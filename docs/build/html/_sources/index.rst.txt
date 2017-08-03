3fdb4b3ca045f9e0d2b18c8045b4f17deeb89e23
Connor Peet <connor@peet.io>
2017-08-03 10:49:55 -0700

    docs(build): regen docs

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,27 @@
+.. interactive-python documentation master file, created by
+   sphinx-quickstart on Tue Aug  1 18:55:17 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Mixer Interactive Python API
+============================
+
+This API is a reference implementation for connecting to Mixer's Interactive service as a game client. This documentation and library is stable, but is still actively developed and further expansions are planned.
+
+Contents:
+
+.. toctree::
+    :maxdepth: 2
+
+    api
+    examples
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['a_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -80,7 +80,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['a_build']
+exclude_patterns = ['../docs']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -144,7 +144,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['a_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
